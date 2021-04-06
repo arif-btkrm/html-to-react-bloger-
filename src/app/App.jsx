97652518353ca7c28aac95aab1793618ac623ca1
arif-btkrm <arif.btkrm@gmail.com>
2021-04-06 13:08:07 +0600

    js library issue fixed

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,28 +3,12 @@ import {Helmet} from "react-helmet";
 import AppRouter from "./router";
 
 
-import './../assets/styles/style.css';
-import './../assets/styles/bootstrap.css'
-
-
-
-
-
-// import './../assets/js/bootstrap';
-// import './../assets/js/custom';
-// import './../assets/js/html5shiv';
-// import './../assets/js/jquery';
-
 function App() {
   return (<div className='App'>
     <Helmet>
       
-      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600" />
-  {/* Not Working */}
+      {/* <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600" rel="stylesheet"/> */}
       
-    {/*  
-     */}
-     {/* <script>try{Typekit.load({ async: true })}catch(e){}</script> */}
     </Helmet>
     
     <AppRouter/>
